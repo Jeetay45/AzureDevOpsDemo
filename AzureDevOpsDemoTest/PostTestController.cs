d97ff2f1e4ec98ed2b61f7d746f15b5eebeaa6c7
Jitendra Kumar <innovativejeetay@gmail.com>
2019-11-26 14:02:45 +0530

    add new test file

--- AzureDevOpsDemoTest/PostTestController.cs
+++ AzureDevOpsDemoTest/PostTestController.cs
@@ -17,7 +17,7 @@ namespace AzureDevOpsDemoTest
         public void MultiplyTest()
         {
             var result = MyMathsClass.Multiply(2, 3);
-            Assert.True(result > 0);
+            Assert.True(result > 0); 
         }
 
         [Fact]
---
+++ AzureDevOpsDemoTest/SecondPostController.cs
@@ -0,0 +1,18 @@
+﻿using AzureDevOpsDemo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AzureDevOpsDemoTest
+{
+    public class SecondPostController
+    {
+        [Fact]
+        public void MultiplyTest()
+        {
+            var result = MyMathsClass.Multiply(2, 3);
+            Assert.True(result > 0);
+        }
+    }
+}
