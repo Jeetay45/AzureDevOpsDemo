96cb4d2f5cdcecc15da6cb6e1f9b65df2cc5337b
Jitendra Kumar <innovativejeetay@gmail.com>
2019-11-26 13:55:06 +0530

    commit test cases

---
+++ AzureDevOpsDemo/MyMathsClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsDemo
+{
+    public static class MyMathsClass
+    {
+        public static int Add(int a, int b)
+        {
+
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+
+            return a + b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+
+            return a/b;
+        }
+    }
+}
--- AzureDevOpsDemoTest/PostTestController.cs
+++ AzureDevOpsDemoTest/PostTestController.cs
@@ -1,3 +1,4 @@
+using AzureDevOpsDemo;
 using System;
 using Xunit;
 
@@ -6,9 +7,31 @@ namespace AzureDevOpsDemoTest
     public class PostTestController
     {
         [Fact]
-        public void Test1()
+        public void AddTest()
         {
+            var result = MyMathsClass.Add(12, 23);
+            Assert.True(result > 0);
+        }
 
+        [Fact]
+        public void MultiplyTest()
+        {
+            var result = MyMathsClass.Multiply(2, 3);
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void DivideTest()
+        {
+            var result = MyMathsClass.Divide(12, 3);
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void FailTest()
+        {
+            var result = MyMathsClass.Divide(1, 3);
+            Assert.True(result > 100);
         }
     }
 }
