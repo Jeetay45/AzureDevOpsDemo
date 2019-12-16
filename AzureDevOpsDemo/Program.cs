23c073f39155341acc9bba385a0ae2f87a5b95f2
Jitendra Kumar <innovativejeetay@gmail.com>
2019-12-16 12:16:37 +0530

    commit remove files

--- AzureDevOpsDemo/Program.cs
+++ AzureDevOpsDemo/Program.cs
@@ -19,6 +19,6 @@ namespace AzureDevOpsDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>(); 
     }
 }
