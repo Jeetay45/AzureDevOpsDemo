de69f1626e7cfcd80ae88f0cdd10bcab04c1bb12
Jitendra Kumar <innovativejeetay@gmail.com>
2019-12-18 16:54:43 +0530

    commit in api and class files

--- AzureDevOpsAPI/Controllers/ValuesController.cs
+++ AzureDevOpsAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AzureDevOpsAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1 2", "value2 1" };
         }
 
         // GET api/values/5
