a58b68942d4436a94f47d734d33534b1357306b9
Jitendra Kumar <innovativejeetay@gmail.com>
2019-12-18 19:37:20 +0530

    changes

--- SupraESApi/Controllers/WeatherForecastController.cs
+++ SupraESApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SupraESApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing 12", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
