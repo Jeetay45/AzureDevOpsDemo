e660e2fcce27f80dc705f83ba888136c52e2269b
Jitendra Kumar <innovativejeetay@gmail.com>
2019-12-18 13:20:56 +0530

    to check

--- AzureDevOpsAPI/Startup.cs
+++ AzureDevOpsAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace AzureDevOpsAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-        }
+        } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
