9a41dff0fb5f95388c1c3ea18cc6114871e15961
Jitendra Kumar <innovativejeetay@gmail.com>
2019-12-19 16:40:58 +0530

    updaetd commit.

--- SupraESApi/Controllers/ValuesController.cs
+++ SupraESApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SupraESApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1 jk ", "value2 Pl" };
         }
 
         // GET api/values/5
